d60491bbe9b0395e435848f422bbdb4decb222a0
Mayank <126972040+MayankDevil@users.noreply.github.com>
2023-10-16 11:35:06 +0000

    version-1.0

--- js/script.js
+++ js/script.js
@@ -1,113 +1,30 @@
 /*
--	website-7 "LearnCoreJava"
--	Developed | Designed by Mayank
--	All Right Reserved by Mayank
--	JavaScript : ./js/script.js
-*/
+    HRvoid scriptsheet
+*/ 
 try
 {
-    let main = document.getElementById('main')
+    let main = document.getElementById('main');
+    let menu = document.getElementById('aside');
 
-    let aside = document.getElementById('aside')
-
-    var active = true
     
-    //document.getElementById('content_frame').src="file:///home/user/Pictures/z8.jpg"
-
-	/*
-		[ menu open function ] -------------------------------------
-	*/
-    function menu_open()
-    {
-        aside.style.display = "block"
-
-        active = true
+    var active = true;
+    
+    function menu_open(){
+        menu.style.display = "block";
+        main.style.gridTemplateColumns = "3fr 1fr";
+        active = true;
     }
-    /*
-		[ menu close function ] ------------------------------------
-    */
-    function menu_close()
-    {
-        aside.style.display = "none"
-
-        active = false
+    function menu_close(){
+        menu.style.display = "none";
+        main.style.gridTemplateColumns = "1fr";
+        active = false;
     }
-
-    /*
-        -----------
-        | display | set sourceFileData into aside anchor
-        -----------
-    */
-
-    sourceFileData.forEach( element => {
-           
-        var souceFileURL = `./data/html/${element}.html`
-        
-        var strings = element
-                
-        let anchor = strings.charAt(0).toUpperCase() + strings.slice(1);
-        
-        anchor = anchor.split('_').join(" ")
-            
-        aside.insertAdjacentHTML("beforeend",`<a href='${souceFileURL}' target="myFrame"> ${ anchor } </a>`)
-    })
-
-    /*
-    	-----------------------------------------------------------
-    	| menu button function | onclick close else open
-    	-----------------------------------------------------------
-   	*/
-
+    // menu button function
     document.getElementById('menu_button').onclick = () => (active)? menu_close() : menu_open();
-    
-    /*
-    	-----------------------------------------------------------
-		| aside anchore function | onclick close menu
-		-----------------------------------------------------------
-    */
-    
-    document.querySelectorAll('#aside > a').forEach(function(i)
-    {
-        i.onclick = () => menu_close()
-    });
-    
-    /*
-        ----------------------
-        [ SEARCHBAR FUNCTION ] ========================================
-        ----------------------
-    */
-    
-    let searchBar = document.getElementById('searchBar')
-
-    let asidelinks = document.querySelectorAll('#aside  a')
-    
-    // search function : on key press loop to match value to anchor
-    
-    searchBar.onkeyup = function()
-    {    
-        let search = searchBar.value.toUpperCase();
-
-        for(let i = 0; i < asidelinks.length; i++)
-        {
-            let a = asidelinks[i];
-
-            let data = a.textContent || a.innerHTML;
-
-            if(data.toUpperCase().indexOf(search) > -1)
-            {
-                asidelinks[i].style.display = "block";
-            }
-            else
-            {
-                asidelinks[i].style.display = "none";
-            }
-        }
-    }
-    
-    document.title = "Mayank"
+    // menu anchor function
+    document.querySelectorAll('#aside > a').forEach(i => { i.onclick = () => menu_close() });
 }
-catch(error)
+catch(e)
 {
-    console.log(`${error}`)
+    console.log(e);
 }
-// the end
